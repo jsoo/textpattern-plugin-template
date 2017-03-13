c8e53526261cb7f5b6e5c2da92ecf5881e958e25
Jeff Soo <jeff@ipsedixit.net>
2017-03-13 05:10:40 -0400

    Reconfigured to allow using the parser(s) in a Textpattern installation

--- config-dist.php
+++ config-dist.php
@@ -1,33 +1,36 @@
 <?php
 
 // Configuration file for the zem_tpl.php Textpattern plugin compiler.
-// This is modified from the official Textpattern plugin template:
+// This is modified from the official Textpattern plugin template,
 // https://github.com/textpattern/textpattern-plugin-template
-// 
-// There are three general ways of setting this up. The compiler should work
+
+// There are several ways of setting this up. The compiler should work
 // running standalone, for which you do not need to change this file. However,
-// the plugin's help text will pass through in raw form.
-//
-// You can run the compiler in conjunction with a Textpattern installation
-// by choosing one of these three options:
-//  a) Copy a full single-file version of classTextile.php to your plugin directory.
-//  b) Uncomment the following line and edit it to give the location where
-//     classTextile.php can be found
-#ini_set('include_path', ini_get('include_path') . ':/full/path/to/textile');
-//  c) Uncomment the following line and edit it to give the location of the textpattern
-//     directory inside a nearby, full Textpattern installation.
+// the plugin's help text will pass through in raw form. 
+
+// To process Textile and Markdown help text, the compiler needs to know how to 
+// find the PHP-Textile and Parsedown packages. PHP-Textile is part of the 
+// default Textpattern installation from version 4.6 on. If you want Markdown 
+// parsing you will have to install Parsedown yourself (link below).
+
+// The simplest configuration is to run the compiler in conjunction with a 
+// Textpattern installation (version 4.6 or newer), by uncommenting the 
+// following line and editing it to give the path to the installation's
+// textpattern directory:
+
 #define('txpath', '/full/path/to/textpattern');
-// 
-// However, there's really no point using this version of the compiler if you
-// want to run it in one of the above configurations; you might as well use
-// the official version. 
-//
-// To fully enable this version, install the PHP-Textile parser and/or the
-// Parsedown parser, then uncomment and edit the appropriate line(s) below. 
+
+// and, optionally, copying the parsedown directory (or a symbolic link pointing
+// to it) to textpattern/vendors.
+
+// Or you can bypass Textpattern and install Parsedown and PHP-Textile in the 
+// locations of your choice, then uncomment and edit the following lines:
+
+#define('PATH_TO_TEXTILE', '/path/to/php-textile/src/Netcarver/Textile');
+#define('PATH_TO_PARSEDOWN', '/path/to/parsedown/Parsedown.php');
+
 // (NB: Parsedown is a parser for GitHub-flavoured Markdown.)
 // https://github.com/erusev/parsedown
 // https://github.com/textile/php-textile
-#define('PATH_TO_TEXTILE', '/path/to/php-textile/src/Netcarver/Textile');
-#define('PATH_TO_PARSEDOWN', '/path/to/parsedown/Parsedown.php');
 
 ?>
--- zem_tpl.php
+++ zem_tpl.php
@@ -36,7 +36,6 @@ function compile_plugin($file='') {
         $plugin['name'] = basename($file, '.php');
     }
 
-    // Read the contents of this file, and strip line ends.
     $content = file($file);
     for ($i=0; $i < count($content); $i++) {
         $content[$i] = rtrim($content[$i]);
@@ -44,54 +43,64 @@ function compile_plugin($file='') {
 
     $plugin['help'] = trim(extract_section($content, 'HELP'));
     $plugin['code'] = extract_section($content, 'CODE');
+    // Textpattern will textile it, and encode html.
+    $plugin['help_raw'] = $plugin['help'];
     
     if (! $plugin['help'] && $plugin['allow_html_help'] && ! empty($compiler_cfg['parser'])) {
         if (isset($compiler_cfg['helpfile'])) {
-            $first_frame = array_pop(debug_backtrace());
+            $backtrace = debug_backtrace();
+            $first_frame = array_pop($backtrace);
             $helpfile = dirname($first_frame['file']).'/'.$compiler_cfg['helpfile'];
             $content = file($helpfile);
             for ($i=0; $i < count($content); $i++) {
                 $content[$i] = rtrim($content[$i]);
             }
             $plugin['help'] = trim(join("\n", $content));
+            $plugin['help_raw'] = $plugin['help'];
         }
         
         if ($plugin['help']) {
-            if ($compiler_cfg['parser'] == 'textile' && defined('PATH_TO_TEXTILE')) {
-                require PATH_TO_TEXTILE.'/Parser.php';
-                require PATH_TO_TEXTILE.'/DataBag.php';
-                require PATH_TO_TEXTILE.'/Tag.php';
-                $parser = new \Netcarver\Textile\Parser('html5');
-                $plugin['help'] = $parser->parse($plugin['help']);
-            } elseif ($compiler_cfg['parser'] == 'parsedown' && defined('PATH_TO_PARSEDOWN')) {
-                require PATH_TO_PARSEDOWN;
-                $parser = new Parsedown();
-                $plugin['help'] = $parser->text($plugin['help']);
+            if (defined('txpath')) {
+                global $trace;
+                include txpath.'/lib/class.trace.php';
+                include txpath.'/vendors/Textpattern/Loader.php';
+                include txpath.'/lib/txplib_misc.php';
+                $trace = new Trace();
+                $loader = new \Textpattern\Loader(txpath.'/vendors');
+                $loader->register();
+                $loader = new \Textpattern\Loader(txpath.'/vendors/parsedown');
+                $loader->register();
+                $loader = new \Textpattern\Loader(txpath.'/lib');
+                $loader->register();
+            } else {
+                if (defined('PATH_TO_TEXTILE')) {
+                    require PATH_TO_TEXTILE.'/Parser.php';
+                    require PATH_TO_TEXTILE.'/DataBag.php';
+                    require PATH_TO_TEXTILE.'/Tag.php';
+                }
+                if (defined('PATH_TO_PARSEDOWN')) {
+                    require PATH_TO_PARSEDOWN;
+                }
+            }
+            if ($compiler_cfg['parser'] == 'textile') {
+                if (class_exists('Netcarver\\Textile\\Parser')) {
+                    $parser = new \Netcarver\Textile\Parser('html5');
+                    $plugin['help'] = $parser->parse($plugin['help']);
+                }
+            } elseif ($compiler_cfg['parser'] == 'parsedown') {
+                if (class_exists('Parsedown')) {
+                    $parser = new Parsedown();
+                    $plugin['help'] = $parser->text($plugin['help']);
+                }
             }
         }
     }
-
-    // Textpattern will textile it, and encode html.
-    $plugin['help_raw'] = $plugin['help'];
-
     // This is for bc; and for help that needs to use it.
-    @include('classTextile.php');
-
-    if (defined('txpath')) {
-        global $trace;
-
-        include txpath.'/lib/class.trace.php';
-        include txpath.'/vendors/Textpattern/Loader.php';
-
-        $trace = new Trace();
-        $loader = new \Textpattern\Loader(txpath.'/lib');
-        $loader->register();
-    }
-
-    if (class_exists('Textile')) {
-        $textile = new Textile();
-        $plugin['help'] = $textile->TextileThis($plugin['help']);
-    }
+//     @include('classTextile.php');
+//     if (class_exists('Textile')) {
+//         $textile = new Textile();
+//         $plugin['help'] = $textile->TextileThis($plugin['help']);
+//     }
 
     $plugin['md5'] = md5( $plugin['code'] );
 
